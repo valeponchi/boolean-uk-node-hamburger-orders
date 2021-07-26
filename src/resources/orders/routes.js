eb5c1167792bed828b47472b2ccc8a7e35c2c62e
valeponchi <81304855+valeponchi@users.noreply.github.com>
2021-07-26 10:57:37 +0100

    validation done with sergio

--- src/resources/orders/routes.js
+++ src/resources/orders/routes.js
@@ -8,28 +8,33 @@ const hamburgers = require('../hamburgers/routes')
 
 let orders = []
 
-//validations
+//VALIDATION OF THE ORDER
 function validateOrder(incomingOrder) {
 	// filter gives back the singleOrder if the f return is true, otherwise it does not return a/t
 	// filter if doesn't find a/t, will give back an empty[]
 	// !! return true or false (no need to the last return then)
 	// return !!incomingOrder.filter(singleOrder => {
+
+	//FIND IF THE HAMBURGER EXISTS
 	const filterResult = incomingOrder.filter(singleOrder => {
 		const foundHamburger = hamburgers.find(
 			hamburger => hamburger.id === singleOrder.hamburger_id
 		)
-		return foundHamburger.quantity >= singleOrder.quantity
-		// return foundHamburger
-		// ? foundHamburger.quantity >= singleOrder.quantity
-		// : false
+		// VALIDATE QUANTITY
+		// return foundHamburger.quantity >= singleOrder.quantity
+		return foundHamburger
+			? foundHamburger.quantity >= singleOrder.quantity
+			: false
 	})
 	return filterResult.length === incomingOrder.length
 }
 
+// GET
 orderRouter.get('/', (req, res) => {
 	res.json({ orders })
 })
 
+// POST
 orderRouter.post('/', (res, res) => {
 	const orderFromClient = req.body
 
@@ -37,15 +42,17 @@ orderRouter.post('/', (res, res) => {
 	// const newId = orders.length + 1
 	// but if you delete one, there will be duplications
 
-	const newOrder = {
-		order_id: newId,
-		items: orderFromClient,
-		status: 'pending',
-		created_at: Date.now(),
-	}
+	if (validateOrder(orderFromClient)) {
+		const newOrder = {
+			order_id: newId,
+			items: orderFromClient,
+			status: 'pending',
+			created_at: Date.now(),
+		}
 
-	orders = [...orders, newOrder]
-	req.json(newOrder.items)
+		orders = [...orders, newOrder]
+		req.json({ newOrder })
+	}
 })
 
 module.exports = orderRouter
